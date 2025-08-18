2e51a9ff99b10b798567e0cbf1bf014da4e18355
Nisha <nishathakur206666@gmail.com>
2025-08-18 21:10:55 +0530

    fix issue

--- backend/controller/user/userSignIn.js
+++ backend/controller/user/userSignIn.js
@@ -35,7 +35,8 @@ async function userSignInController(req,res){
 
         const tokenOption = {
             httpOnly : true,
-            secure : true
+            secure : true,
+            sameSite : "none", // Important for cross-origin cookies
         }
 
         res.cookie("token",token,tokenOption).status(200).json({
